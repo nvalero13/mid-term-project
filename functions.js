749ad2fe5e5433ce19b6633faf0b84bda7eafb9d
Nil Valero <nvalero13@gmail.com>
2022-11-15 21:11:37 +0100

    add nav and footer import

--- functions.js
+++ functions.js
@@ -1,15 +1,44 @@
-document.querySelector(".nav-burger").addEventListener("click", toggleMenu)
+window.addEventListener("load", (event) => {
+  setActive();
+});
 
-let toggled = false
+function setActive() {
+  let url = window.location.href;
+  let urlSplit = url.split("/");
+  let page = urlSplit[urlSplit.length - 1];
+console.log(page)
+  $("body a").removeClass("active");
+
+  switch (page) {
+    case "home.html":
+      $(`#nav-home li`).addClass("active");
+      break;
+    case "project.html":
+      $(`#nav-projects li`).addClass("active");
+      break;
+    case "contact.html":
+      $(`#nav-contact li`).addClass("active");
+      break;
+  }
+  console.log("Hi");
+}
+
+document.querySelector(".nav-burger").addEventListener("click", toggleMenu);
+
+let toggled = false;
 function toggleMenu() {
-    if(!toggled) {
-        document.querySelector(".nav-burger img").setAttribute("src","project-assets/nav/burguer-icon.svg")
+  if (!toggled) {
+    document
+      .querySelector(".nav-burger img")
+      .setAttribute("src", "project-assets/nav/burguer-icon.svg");
     document.querySelector(".nav-info").style.transform = "translateY(-200px)";
     toggled = true;
-    } else {
-        document.querySelector(".nav-burger img").setAttribute("src","project-assets/nav/cross-icon.svg")
- 
-    document.querySelector(".nav-info").style.transform = "translateY(50px)"; 
+  } else {
+    document
+      .querySelector(".nav-burger img")
+      .setAttribute("src", "project-assets/nav/cross-icon.svg");
+
+    document.querySelector(".nav-info").style.transform = "translateY(50px)";
     toggled = false;
-    }
+  }
 }
