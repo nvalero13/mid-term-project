d82cfd269e56214e545af120f57b42c88b6a66fd
Nil Valero <nvalero13@gmail.com>
2022-11-15 11:08:21 +0100

    add nav toggle

---
+++ functions.js
@@ -0,0 +1,16 @@
+document.querySelector(".nav-burger").addEventListener("click", toggleMenu)
+
+let toggled = false
+function toggleMenu() {
+    
+    if(!toggled) {
+        document.querySelector(".nav-burger img").setAttribute("src","project-assets/nav/burguer-icon.svg")
+    document.querySelector(".nav-info").style.transform = "translateY(-200px)";
+    toggled = true;
+    } else {
+        document.querySelector(".nav-burger img").setAttribute("src","project-assets/nav/cross-icon.svg")
+ 
+    document.querySelector(".nav-info").style.transform = "translateY(50px)"; 
+    toggled = false;
+    }
+}
